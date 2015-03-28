ee126df96faeb75c2562d6fd006474213bf694db
haracane <haracane@gmail.com>
2015-03-28 12:04:04 +0900

    Add sns count cache

--- _plugins/ranking.rb
+++ _plugins/ranking.rb
@@ -13,6 +13,7 @@ class Jekyll::Post
   end
 
   def get_response_body(uri)
+    # return nil if site.config['show_drafts']
     begin
       Net::HTTP.get_response(URI.parse(uri)).body
     rescue StandardError
@@ -24,24 +25,60 @@ class Jekyll::Post
     return data['hatena_count'] if data['hatena_count']
     uri = "http://api.b.st-hatena.com/entry.count?url=#{full_url}"
 
+    cache_key = 'hatena_counts'
+    cached_count = fetch_cached_count(cache_key, full_url)
+
     body = get_response_body(uri)
-    data['hatena_count'] = body && body.to_i || 0
+    latest_count = body ? body.to_i : cached_count
+
+    save_cache_on_update(cache_key, full_url, latest_count, cached_count)
+
+    data['hatena_count'] = latest_count || cached_count || 0
   end
 
   def facebook_count
     return data['facebook_count'] if data['facebook_count']
     uri = "http://graph.facebook.com/#{full_url}"
 
+    cache_key = 'facebook_counts'
+    cached_count = fetch_cached_count(cache_key, full_url)
+
     body = get_response_body(uri)
-    data['facebook_count'] = body && JSON.parse(body)['shares'] || 0
+    latest_count = body ? JSON.parse(body)['shares'] : cached_count
+
+    save_cache_on_update(cache_key, full_url, latest_count, cached_count)
+
+    data['facebook_count'] = latest_count || cached_count || 0
   end
 
   def twitter_count
     return data['twitter_count'] if data['twitter_count']
     uri = "http://urls.api.twitter.com/1/urls/count.json?url=#{full_url}"
 
+    cache_key = 'twitter_counts'
+    cached_count = fetch_cached_count(cache_key, full_url)
+
     body = get_response_body(uri)
-    data['twitter_count'] = body && JSON.parse(body)['count'] || 0
+    latest_count = body ? JSON.parse(body)['count'] : cached_count
+
+    save_cache_on_update(cache_key, full_url, latest_count, cached_count)
+
+    data['twitter_count'] = latest_count || cached_count || 0
+  end
+
+  private
+
+  def fetch_cached_count(cache_key, url)
+    cache = JSON.parse(File.read('_data/cache.json'))
+    site.data['cache'][cache_key] ||= {}
+    cached_count = site.data['cache'][cache_key][full_url]
+  end
+
+  def save_cache_on_update(cache_key, url, latest_count, cached_count)
+    if latest_count && latest_count != cached_count
+      site.data['cache'][cache_key][url] = latest_count
+      File.write('../template.enogineer.com/_data/cache.json', site.data['cache'].to_json)
+    end
   end
 end
 
@@ -55,12 +92,6 @@ module Jekyll
 
       begin
         site.posts.each do |post|
-          if site.config['show_drafts']
-            post.data['hatena_count'] = rand(100)
-            post.data['facebook_count'] = rand(100)
-            post.data['twitter_count'] = rand(100)
-          end
-
           score =
             WEIGHT_OF_HATENA * post.hatena_count +
             WEIGHT_OF_FACEBOOK * post.facebook_count +
